05df89c126d5b376b3dfa306ec501afa2b5e5b4a
Simon Goller <neosam+github@posteo.de>
2024-06-13 07:53:29 +0200

    Add favicon and logo

--- src/page/home.rs
+++ src/page/home.rs
@@ -1,19 +1,21 @@
 use dioxus::prelude::*;
 
 use crate::component::TopBar;
+use crate::router::Route;
 use crate::state;
 
 #[component]
 pub fn Home() -> Element {
     let mut count = use_signal(|| 0);
     let backend = use_context::<state::Config>().backend.clone();
+    let nav = navigator();
+    nav.push(Route::ShiftPlan {});
 
     rsx! {
         TopBar {}
         div {
-            h1 { "High-Five counter: {count} {backend}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
+            h1 { "Welcome to Shifty!" }
+            p { "" }
         }
     }
 }
