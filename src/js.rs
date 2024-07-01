817317ed4761c5c48784bd020ca30f357e43054f
Simon Goller <neosam+github@posteo.de>
2024-07-01 10:09:52 +0200

    Fix determining current week

--- src/js.rs
+++ src/js.rs
@@ -7,18 +7,7 @@ pub fn get_current_year() -> u32 {
 
 // Function to get the current week number based on ISO 8601
 pub fn get_current_week() -> u8 {
-    let date = Date::new_0();
-
-    // Get the first day of the year
-    let start_of_year = Date::new_with_year_month_day(date.get_full_year(), 0, 1);
-    let day_of_year = ((date.get_time() - start_of_year.get_time()) / (1000.0 * 60.0 * 60.0 * 24.0))
-        .floor() as u32
-        + 1;
-
-    // Calculate ISO week number
-    let week_number = (day_of_year + 6 - (start_of_year.get_day() as u32)) / 7;
-
-    week_number as u8
+    current_datetime().iso_week()
 }
 
 pub fn js_date_to_primitive_date_time(date: &Date) -> time::PrimitiveDateTime {
