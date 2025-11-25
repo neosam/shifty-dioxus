c4af3845e810ffd246f0a1a30ec5ad318a8da5bf
Simon Goller <neosam+github@posteo.de>
2025-11-25 12:05:26 +0100

    chore: version 1.5.2

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
         };
         frontend-build = pkgs.rustPlatform.buildRustPackage rec {
           pname = "shifty-dioxus";
-          version = "1.5.1";
+          version = "1.5.2";
           
           src = ./.;
           
