a09fe3e09d7b823a30ad6fa50ae8b37910b2067d
Simon Goller <neosam+github@posteo.de>
2026-03-13 19:19:59 +0100

    Set version to 1.9.0

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
         };
         frontend-build = pkgs.rustPlatform.buildRustPackage rec {
           pname = "shifty-dioxus";
-          version = "1.9.0-dev";
+          version = "1.9.0";
           
           src = ./.;
           
